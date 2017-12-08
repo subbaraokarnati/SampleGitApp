c18ad5fc4dfa04d0531a93f518af008346014f20
KA376091 <KA376091@D-113074638.wipro.com>
2017-12-08 17:31:18 +0530

    initial check in sample git application

---
+++ SampleGitApplication/src/com/git/Employee.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class Employee {
+
+	public void studentDetails(){
+		System.out.println("Enter the name:");
+		System.out.println("Enter the salary:");
+		System.out.println("Enter the address:");
+		System.out.println("Enter the company:");
+	}
+}
---
+++ SampleGitApplication/src/com/git/Main.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.studentDetails();
+		System.out.println();
+		System.out.println("subbarao");
+		System.out.println("chennai");
+		
+	}
+}
